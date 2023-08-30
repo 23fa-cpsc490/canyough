5e931d1f9d28634d11718dcc14dc5a9ed414c3e3
diamondburned <diamond@arikawa-hi.me>
2023-08-29 23:10:55 -0700

    Fix contributors.sh, add commit counts

--- contributors.sh
+++ contributors.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # This script prints the list of contributors to the repository.
 # Usage: ./contributors.sh
-git log --pretty=format:"%an" | sort -u
+git log --pretty=format:"%an" | sort | uniq -c | sort -rn
